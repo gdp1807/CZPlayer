2157e4481f23b89e62ed2eb7008ded3a558d0539
gdp1807 <30888751+gdp1807@users.noreply.github.com>
2018-11-28 11:50:00 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,4 +3,5 @@ import sys
 sudo_pass = sys.argv[1]
 username = sys.argv[2]
 os.system('echo ' + sudo_pass + ' | sudo -S mv /usr/local/CZPlayer/czplayer.py /usr/local/bin/czplayer')
+os.system('echo ' + sudo_pass + ' | sudo -S mkdir /usr/local/CZPlayer/data') 
 os.system('echo ' + sudo_pass + ' | sudo -S chown -R ' + username + ' /usr/local/CZPlayer')
